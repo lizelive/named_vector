46c22703c266ab3c9beab6ba96f0d08c88f978f3
lizelive <git@lize.live>
2026-01-08 10:18:30 -0800

    create struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,36 @@
 #[macro_export]
 macro_rules! named_vector {
-    ($(#[$enum_meta:meta])* $vis:vis enum $name:ident { $($(#[$variant_meta:meta])* $variant:ident),+ $(,)? }) => {
+    ($(#[$enum_meta:meta])* $vis:vis vec $name:ident { $($(#[$axes_meta:meta])* $axes:ident),+ $(,)? }) => {
         $(#[$enum_meta])*
-        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-        $vis enum $name {
-            $($(#[$variant_meta])* $variant),+
+        $vis struct $name<T=f32>{
+            $($(#[$axes_meta])* pub $axes : T),+
         }
         paste::paste! {
-            impl $name {
+            $(#[$enum_meta])*
+            #[repr(u8)]
+            #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+            $vis enum [<$name Axes>]  {
+                #[default]
+                $($(#[$axes_meta])* [<$axes:camel>],)+
+                Count
+            }
+            
+            impl [<$name Axes>] {
                 $(
-                    pub fn [<is_ $variant:snake>](self) -> bool {
-                        self == Self::$variant
+                    pub fn [<is_ $axes>](self) -> bool {
+                        self == Self::[<$axes:camel>]
                     }
                 )*
-                /// A slice containing all variants of the enum.
-                pub const VALUES: &'static [Self] = &[
-                    $(Self::$variant),+
+
+                pub const COUNT: usize = Self::Count as usize;
+                /// A slice containing all axess of the enum.
+                pub const VALUES: [Self; Self::COUNT] = [
+                    $(Self::[<$axes:camel>]),+
                 ];
 
-                /// Returns a slice with all variants.
-                pub fn values() -> &'static [Self] {
-                    Self::VALUES
+                /// Returns a slice with all axess.
+                pub const fn values() -> &'static [Self] {
+                    & Self::VALUES
                 }
             }
         }
@@ -30,22 +40,43 @@ macro_rules! named_vector {
 #[cfg(test)]
 mod tests {
     named_vector! {
-        #[derive(Default)]
-        pub enum UwU {
+        pub vec UwU {
             /// hello world
-            #[default]
-            A,
-            B,
-            C,
+            x,
+            y,
+            z,
         }
     }
     #[test]
-    fn uwu_values_contains_all_variants() {
-        assert_eq!(UwU::values(), &[UwU::A, UwU::B, UwU::C]);
+    fn uwu_values_contains_all_axess() {
+        assert_eq!(UwUAxes::values(), &[UwUAxes::X, UwUAxes::Y, UwUAxes::Z]);
     }
 
     #[test]
     fn uwu_values_len() {
-        assert_eq!(UwU::values().len(), 3);
+        assert_eq!(UwUAxes::values().len(), 3);
+    }
+    #[test]
+    fn uwu_count_const() {
+        assert_eq!(UwUAxes::COUNT, 3);
+        assert_eq!(UwUAxes::values().len(), UwUAxes::COUNT);
+    }
+
+    #[test]
+    fn uwu_is_methods() {
+        // X axes
+        assert!(UwUAxes::X.is_x());
+        assert!(!UwUAxes::X.is_y());
+        assert!(!UwUAxes::X.is_z());
+
+        // Y axes
+        assert!(!UwUAxes::Y.is_x());
+        assert!(UwUAxes::Y.is_y());
+        assert!(!UwUAxes::Y.is_z());
+
+        // Z axes
+        assert!(!UwUAxes::Z.is_x());
+        assert!(!UwUAxes::Z.is_y());
+        assert!(UwUAxes::Z.is_z());
     }
 }
