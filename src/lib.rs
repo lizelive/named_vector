a1c0c9e305d63a204d59b41295986b8349fe9546
lizelive <git@lize.live>
2026-01-08 10:26:29 -0800

    only use paste when needed

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ macro_rules! named_vector {
                 $($(#[$axes_meta])* [<$axes:camel>],)+
                 Count
             }
-            
+
             impl [<$name Axes>] {
                 $(
                     pub fn [<is_ $axes>](self) -> bool {
@@ -34,84 +34,83 @@ macro_rules! named_vector {
                     & Self::VALUES
                 }
             }
+        }
+        // ---- arithmetic impls ----
+        impl<T> ::core::ops::Add for $name<T>
+        where T: ::core::ops::Add<Output = T>
+        {
+            type Output = Self;
+            fn add(self, rhs: Self) -> Self::Output {
+                Self { $($axes: self.$axes + rhs.$axes),+ }
+            }
+        }
 
-            // ---- arithmetic impls ----
-            impl<T> ::core::ops::Add for $name<T>
-            where T: ::core::ops::Add<Output = T>
-            {
-                type Output = Self;
-                fn add(self, rhs: Self) -> Self::Output {
-                    Self { $($axes: self.$axes + rhs.$axes),+ }
-                }
+        impl<T> ::core::ops::Sub for $name<T>
+        where T: ::core::ops::Sub<Output = T>
+        {
+            type Output = Self;
+            fn sub(self, rhs: Self) -> Self::Output {
+                Self { $($axes: self.$axes - rhs.$axes),+ }
+            }
+        }
+
+        impl<T> ::core::ops::Mul for $name<T>
+        where T: ::core::ops::Mul<Output = T>
+        {
+            type Output = Self;
+            fn mul(self, rhs: Self) -> Self::Output {
+                Self { $($axes: self.$axes * rhs.$axes),+ }
+            }
+        }
+
+        impl<T> ::core::ops::Div for $name<T>
+        where T: ::core::ops::Div<Output = T>
+        {
+            type Output = Self;
+            fn div(self, rhs: Self) -> Self::Output {
+                Self { $($axes: self.$axes / rhs.$axes),+ }
+            }
+        }
+
+        impl<T> ::core::ops::Neg for $name<T>
+        where T: ::core::ops::Neg<Output = T>
+        {
+            type Output = Self;
+            fn neg(self) -> Self::Output {
+                Self { $($axes: -self.$axes),+ }
+            }
+        }
+
+        // ---- num_traits implementations ----
+        impl<T> ::num_traits::Zero for $name<T>
+        where T: ::num_traits::Zero + Copy + ::core::ops::Add<Output = T>
+        {
+            fn zero() -> Self {
+                Self { $($axes: ::num_traits::Zero::zero()),+ }
             }
 
-            impl<T> ::core::ops::Sub for $name<T>
-            where T: ::core::ops::Sub<Output = T>
-            {
-                type Output = Self;
-                fn sub(self, rhs: Self) -> Self::Output {
-                    Self { $($axes: self.$axes - rhs.$axes),+ }
-                }
+            fn is_zero(&self) -> bool {
+                $( ::num_traits::Zero::is_zero(&self.$axes) )&&+
+            }
+        }
+
+        impl<T> ::num_traits::One for $name<T>
+        where T: ::num_traits::One + Copy + ::core::ops::Mul<Output = T>
+        {
+            fn one() -> Self {
+                Self { $($axes: ::num_traits::One::one()),+ }
+            }
+        }
+
+        impl<T> ::num_traits::Bounded for $name<T>
+        where T: ::num_traits::Bounded + Copy
+        {
+            fn min_value() -> Self {
+                Self { $($axes: ::num_traits::Bounded::min_value()),+ }
             }
 
-            impl<T> ::core::ops::Mul for $name<T>
-            where T: ::core::ops::Mul<Output = T>
-            {
-                type Output = Self;
-                fn mul(self, rhs: Self) -> Self::Output {
-                    Self { $($axes: self.$axes * rhs.$axes),+ }
-                }
-            }
-
-            impl<T> ::core::ops::Div for $name<T>
-            where T: ::core::ops::Div<Output = T>
-            {
-                type Output = Self;
-                fn div(self, rhs: Self) -> Self::Output {
-                    Self { $($axes: self.$axes / rhs.$axes),+ }
-                }
-            }
-
-            impl<T> ::core::ops::Neg for $name<T>
-            where T: ::core::ops::Neg<Output = T>
-            {
-                type Output = Self;
-                fn neg(self) -> Self::Output {
-                    Self { $($axes: -self.$axes),+ }
-                }
-            }
-
-            // ---- num_traits implementations ----
-            impl<T> ::num_traits::Zero for $name<T>
-            where T: ::num_traits::Zero + Copy + ::core::ops::Add<Output = T>
-            {
-                fn zero() -> Self {
-                    Self { $($axes: ::num_traits::Zero::zero()),+ }
-                }
-
-                fn is_zero(&self) -> bool {
-                    $( ::num_traits::Zero::is_zero(&self.$axes) )&&+
-                }
-            }
-
-            impl<T> ::num_traits::One for $name<T>
-            where T: ::num_traits::One + Copy + ::core::ops::Mul<Output = T>
-            {
-                fn one() -> Self {
-                    Self { $($axes: ::num_traits::One::one()),+ }
-                }
-            }
-
-            impl<T> ::num_traits::Bounded for $name<T>
-            where T: ::num_traits::Bounded + Copy
-            {
-                fn min_value() -> Self {
-                    Self { $($axes: ::num_traits::Bounded::min_value()),+ }
-                }
-
-                fn max_value() -> Self {
-                    Self { $($axes: ::num_traits::Bounded::max_value()),+ }
-                }
+            fn max_value() -> Self {
+                Self { $($axes: ::num_traits::Bounded::max_value()),+ }
             }
         }
     }
@@ -162,10 +161,24 @@ mod tests {
 
     #[test]
     fn uwu_zero_and_one() {
-        use ::num_traits::{Zero, One};
-        assert_eq!(UwU::<f32>::zero(), UwU { x: 0.0f32, y: 0.0f32, z: 0.0f32 });
+        use ::num_traits::{One, Zero};
+        assert_eq!(
+            UwU::<f32>::zero(),
+            UwU {
+                x: 0.0f32,
+                y: 0.0f32,
+                z: 0.0f32
+            }
+        );
         assert!(UwU::<f32>::zero().is_zero());
-        assert_eq!(UwU::<f32>::one(), UwU { x: 1.0f32, y: 1.0f32, z: 1.0f32 });
+        assert_eq!(
+            UwU::<f32>::one(),
+            UwU {
+                x: 1.0f32,
+                y: 1.0f32,
+                z: 1.0f32
+            }
+        );
     }
 
     #[test]
@@ -177,11 +190,51 @@ mod tests {
 
     #[test]
     fn uwu_arithmetic() {
-        let a = UwU::<f32> { x: 1.0f32, y: 2.0f32, z: 3.0f32 };
-        let b = UwU::<f32> { x: 4.0f32, y: 5.0f32, z: 6.0f32 };
-        assert_eq!(a + b, UwU::<f32> { x: 5.0f32, y: 7.0f32, z: 9.0f32 });
-        assert_eq!(b - a, UwU::<f32> { x: 3.0f32, y: 3.0f32, z: 3.0f32 });
-        assert_eq!(a * UwU::<f32> { x: 2.0f32, y: 2.0f32, z: 2.0f32 }, UwU::<f32> { x: 2.0f32, y: 4.0f32, z: 6.0f32 });
-        assert_eq!(-a, UwU::<f32> { x: -1.0f32, y: -2.0f32, z: -3.0f32 });
+        let a = UwU::<f32> {
+            x: 1.0f32,
+            y: 2.0f32,
+            z: 3.0f32,
+        };
+        let b = UwU::<f32> {
+            x: 4.0f32,
+            y: 5.0f32,
+            z: 6.0f32,
+        };
+        assert_eq!(
+            a + b,
+            UwU::<f32> {
+                x: 5.0f32,
+                y: 7.0f32,
+                z: 9.0f32
+            }
+        );
+        assert_eq!(
+            b - a,
+            UwU::<f32> {
+                x: 3.0f32,
+                y: 3.0f32,
+                z: 3.0f32
+            }
+        );
+        assert_eq!(
+            a * UwU::<f32> {
+                x: 2.0f32,
+                y: 2.0f32,
+                z: 2.0f32
+            },
+            UwU::<f32> {
+                x: 2.0f32,
+                y: 4.0f32,
+                z: 6.0f32
+            }
+        );
+        assert_eq!(
+            -a,
+            UwU::<f32> {
+                x: -1.0f32,
+                y: -2.0f32,
+                z: -3.0f32
+            }
+        );
     }
 }
